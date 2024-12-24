b522968673eecfb33061bc11956c2b30d0e79231
Shafiqullah Qasemi <shafiqullah.qsm@gmail.com>
2024-12-24 16:00:26 +0100

    Added some urls

--- food_menu/urls.py
+++ food_menu/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
   path("", views.index, name="index"),
+  path("cool/", views.cool, name="cool"),
+  path("item/", views.item, name="item"),
 ]
--- food_menu/views.py
+++ food_menu/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
   return HttpResponse("Hello world!")
+
+def cool(request):
+  return HttpResponse("Cool!!!!!!!!!")
+
+def item(request):
+  return HttpResponse("This is an item view")
