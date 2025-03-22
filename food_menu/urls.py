8ec92d7f0e21deeb537e788145e19a0fc3d01a16
Shafiqullah Qasemi <shafiqullah.qsm@gmail.com>
2025-03-22 17:32:11 +0300

    Implement the crud functionalities

--- food_menu/urls.py
+++ food_menu/urls.py
@@ -8,4 +8,10 @@ urlpatterns = [
   # /food/1
   path("<int:itemId>/", views.detail, name="detail"),
   path("item/", views.item, name="item"),
+  # add item
+  path("add/", views.createItem, name="createItem"),
+  # edit item
+  path("update/<int:itemId>/", views.updateItem, name="updateItem"),
+  # delete item
+  path("delete/<int:itemId>/", views.deleteItem, name="deleteItem"),
 ]
--- food_menu/views.py
+++ food_menu/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from food_menu.item_form import ItemForm
 from .models import Item
 
 # Create your views here.
@@ -19,3 +20,31 @@ def detail(request, itemId):
 
 def item(request):
   return HttpResponse("This is an item view")
+
+def createItem(request):
+  form = ItemForm(request.POST or None)
+
+  if form.is_valid():
+    form.save()
+    return redirect('food_menu:index')
+  
+  return render(request, "food_menu/item_form.html", {"form": form})
+
+def updateItem(request, itemId):
+  item = Item.objects.get(id=itemId)
+  form = ItemForm(request.POST or None, instance=item)
+
+  if form.is_valid():
+    form.save()
+    return redirect('food_menu:index')
+  
+  return render(request, "food_menu/item_form.html", {"form": form, "item": item})
+
+def deleteItem(request, itemId):
+  item = Item.objects.get(id=itemId)
+
+  if request.method == "POST":
+    item.delete()
+    return redirect('food_menu:index')
+  
+  return render(request, "food_menu/item_delete.html", {"item": item})
